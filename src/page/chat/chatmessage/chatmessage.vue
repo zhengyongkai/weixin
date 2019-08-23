e1c6eb6a383a05e9febcc58cecf7ba52904fa94e
zhenguyongkai <825947557@qq.com>
2019-08-23 16:58:15 +0800

    修改聊天框

--- src/page/chat/chatmessage/chatmessage.vue
+++ src/page/chat/chatmessage/chatmessage.vue
@@ -31,7 +31,88 @@
            </div>
         </div>  
         <div class="footer">
-            <footer>dasd</footer>
+            <div class="contents">
+                <svg class="svg_v">
+                  <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#voice"></use>
+                </svg>
+                <textarea class="svg_input" size="20"  @keyup.enter="send($event)" v-model="input_t" />
+                <div class="svg_o">
+                     <svg  class="face">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#face"></use>
+                      </svg>
+                      <svg  class="addthing">
+                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#addthing"></use>
+                      </svg>
+                
+                </div>
+            </div>
+            <div class="items_o">
+                  <div class="item_o">
+                      <div class="svg_i">
+                          <svg  class="svg_p" fill="#7a8187">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#personimg" ></use>
+                          </svg>
+                      </div>
+                      <div class="svg_text">相册</div>
+                  </div>
+                  <div class="item_o">
+                       <div class="svg_i">
+                          <svg  class="svg_p" fill="#7a8187">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shot" ></use>
+                          </svg>
+                      </div>
+                      <div class="svg_text">拍摄</div>
+                  </div>
+                  <div class="item_o">
+                        <div class="svg_i">
+                          <svg  class="svg_p" fill="#7a8187">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#camera" ></use>
+                          </svg>
+                      </div>
+                      <div class="svg_text">视频聊天</div>
+                  </div>
+                  <div class="item_o">
+                        <div class="svg_i">
+                          <svg  class="svg_p" fill="#7a8187">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#positions" ></use>
+                          </svg>
+                      </div>
+                      <div class="svg_text">位置</div>
+                  </div>
+                  <div class="item_o">
+                        <div class="svg_i">
+                          <svg  class="svg_p" fill="#7a8187">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#redbag" ></use>
+                          </svg>
+                      </div>
+                      <div class="svg_text">红包</div>
+                  </div>
+                  <div class="item_o">
+                        <div class="svg_i">
+                          <svg  class="svg_p" fill="#7a8187">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#banktransfer" ></use>
+                          </svg>
+                      </div>
+                      <div class="svg_text">转账</div>
+                  </div>
+                  <div class="item_o">
+                       <div class="svg_i">
+                          <svg  class="svg_p" fill="#7a8187">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#person" ></use>
+                          </svg>
+                      </div>
+                      <div class="svg_text">名片</div>
+                  </div>
+                  <div class="item_o">
+                        <div class="svg_i">
+                          <svg  class="svg_p" fill="#7a8187">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#voiceinput" ></use>
+                          </svg>
+                      </div>
+                      <div class="svg_text">语音输入</div>
+                  </div>
+               
+            </div>
         </div>
     </div>
 </template>
@@ -51,12 +132,41 @@ export default {
                 "zyk":"你在干嘛",
                 "zyk":"有空帮我拿下快递"
             },
-            "2018-5-6 12:06":{
+            "2018-5-6 12:10":{
                 "zyk":"?",
-                "czm":"好的 我晚点去拿"
+                "czm":"好的 我晚点去拿",
+
+            },"2018-5-6 12:12":{
+                "zyk":"?",
+                "czm":"好的 我晚点去拿",
+
+            },"2018-5-6 12:18":{
+                "zyk":"?",
+                "czm":"好的 我晚点去拿",
+
+            },
+            "2018-5-6 12:20":{
+                "zyk":"?",
+                "czm":"好的 我晚点去拿",
+
+            },
+             "2018-5-6 12:20":{
+                "zyk":"?",
+                "czm":"好的 我晚点去拿ka",
+
             },
 
-        }
+        },
+        input_t:""
+    }
+  },
+  methods:{
+    send(event){
+        event.cancelBubble=true;
+
+      event.preventDefault();
+
+      event.stopPropagation();
     }
   }
 }
@@ -69,18 +179,23 @@ body,html{
 
 .body{
   position: relative;
+  
 }
     .top{
         text-align: center;
         position: fixed;
         width: 100%;
         height: 3rem;   
-        background: #fff;
+        background:  rgba(229, 229, 229, 1);
         padding:  0 0.81rem;
         border-bottom: 0.2rem solid #ebebeb;
         line-height: 3rem;
         color:#000;
         z-index: 999;
+        right: 0;
+        
+        top: 0;
+        left:0;
         .top_content{
           position: relative;
         }
@@ -128,9 +243,14 @@ body,html{
     }
 
     .content{
+      
       position: absolute;
-      top:3rem;
+      height: auto;
       width: 100%;
+      margin-top: 3rem;
+      margin-bottom: 3rem;
+     
+    
       .time{
           text-align: center;
           margin-bottom: 0.5rem;
@@ -167,6 +287,8 @@ body,html{
             padding: 0.5rem 0.5rem;
             background:#fff;
             max-width:15rem ;
+            overflow: hidden;
+
 
           }
       }
@@ -202,5 +324,114 @@ body,html{
           }
       }
 
+    
+
     }
+      .footer{
+
+           z-index: 10;
+          position: fixed;
+          bottom:16rem;
+          height:3.06rem;
+          background: rgba(246, 246, 246, 1);
+          width: 100%;
+          left: 0;
+          right: 0;
+         
+
+          .items_o{
+            height:17rem;
+            
+            background: rgba(246, 246, 246, 1);
+            flex-wrap: wrap;
+            bottom: 0;
+            display: flex;
+           justify-content: center; 
+          
+           
+            padding:1.408rem 1.1946666667rem 0;
+            
+            .item_o{
+
+               width: 25%;
+             
+               height:3.8rem;;
+
+
+              
+              
+              .svg_i{
+                margin:0 auto;
+                background: #fff;
+                border-radius: 0.31rem;
+                     
+                   height:3.8rem;;
+                    width: 3.8rem;
+                  
+                    
+                  
+              }
+              .svg_p{
+                text-align: center;
+                margin: 0.95rem;
+                width: 1.9rem;
+                height: 1.39rem;
+              }
+              .svg_text{
+                
+                width: 3.8rem;
+                margin:0 auto;
+                text-align: center;
+                margin-top:0.5rem;
+                font-size: 0.73rem;
+                color:rgba(136, 136, 136, 1);
+              }
+              
+            }
+          }
+         
+          .contents{
+               height:3.06rem;
+              padding: 0.56rem 0.69rem;
+              border-bottom: 0.1rem solid rgba(220, 220, 220, 1);
+              .svg_v,.svg_o{
+                padding: 0.2rem 0;
+              }
+             .svg_v{
+
+                float: left;
+                height: 1.61rem;
+                width: 1.61rem;
+              }
+              .svg_o{
+                float: right;
+                svg{
+                  height: 1.61rem;
+                   width: 1.61rem;
+                }
+                .addthing{
+                  width: 2rem;
+                }
+              }
+              .svg_input{ 
+                  overflow: hidden;
+                    outline:none;
+                  height: 2rem;
+                  padding: 0 0.5rem;
+                  font-size: 1.2rem;
+                  background: rgba(255, 255, 255, 1);
+                  margin: 0 1rem ;  
+                  border: 0.06rem solid rgba(229, 229, 229, 1);
+                  border-radius: 0.19rem;
+                  line-height: 2rem;
+
+                 
+                
+              }
+
+          }
+          
+      }
+
+
 </style>
